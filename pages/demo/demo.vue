1b92f2a60c27d4ad1daf1320a33ba5c6e5f8c70f
tanFangJie <1023369905@qq.com>
2025-03-04 09:24:11 +0800

    初始化仓库项目并新增注册功能

---
+++ pages/demo/demo.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<view>{{username}}</view>
+		<view v-for="item in tel">{{item}}</view>
+		<view v-if="age >= 18" >
+			<button @tap="signUp()">我要报名</button>
+		</view>
+		<view>
+			<input type="text" v-model="address" placeholder="请输入地址"/>
+		</view>
+		<view>{{address}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				username: "Tan",
+				tel :['110','119'],
+				age :18,
+				address:""
+			}
+		},
+		methods: {
+			signUp:function() {
+				uni.showToast({
+					title:"点击了报名按钮"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<image src="../../static/logo-1.png" class="logo" mode="widthFix"></image>
+		<view class="logo-title">EMOS企业在线办公系统</view>
+		<view class="logo-subtitle">by TanFangJie</view>
+		<button class="login-btn" open-type="getUserInfo" @tap="login()">登录系统</button>
+		<view class="register-container">
+			没有账号？
+			<text class="register" @tap="toRegister()">立刻注册</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			toRegister:function (){
+				uni.navigateTo({
+					url:"../register/register"
+				})
+			},
+			login:function(){
+				
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("login.less");
+</style>
