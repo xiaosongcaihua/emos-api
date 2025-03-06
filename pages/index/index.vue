d92fbca4dbc81d1a6ee7f71ee368eec10d66b4b9
tanFangJie <1023369905@qq.com>
2025-03-06 13:07:35 +0800

    完善index基础页面和css

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,18 +3,86 @@
 		<!-- 循环播放 变化轮播图时间间隔 变化图片花费时间 -->
 		<swiper circular="true" interval="8000" duration="1000" class="swiper">
 			<swiper-item>
-				<image mode="widthFix" src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-1.jpg"></image>
+				<image mode="widthFix"
+					src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-1.jpg"></image>
 			</swiper-item>
 			<swiper-item>
-				<image mode="widthFix" src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-2.jpg"></image>
+				<image mode="widthFix"
+					src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-2.jpg"></image>
 			</swiper-item>
 			<swiper-item>
-				<image mode="widthFix" src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-3.jpg"></image>
+				<image mode="widthFix"
+					src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-3.jpg"></image>
 			</swiper-item>
 			<swiper-item>
-				<image mode="widthFix" src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-4.jpg"></image>
+				<image mode="widthFix"
+					src="https://emos-1320704241.cos.ap-guangzhou.myqcloud.com/img/banner/swiper-4.jpg"></image>
 			</swiper-item>
 		</swiper>
+		<view class="notify-container">
+			<view class="notify-title">
+				<image class="notify-icon" mode="widthFix" src="../../static/icon-1.png"></image>
+				消息提醒
+			</view>
+			<view class="notify-content">你有{{ unreaderRows}} 条消息未读</view>
+			<image class="more-icon" mode="widthFix" src="../../static/icon-2.png"></image>
+		</view>
+		<view class="nav-container">
+			<view class="nav-row">
+				<view class="nav">
+					<image src="../../static/nav-1.png" mode="widthFix" class="icon"></image>
+					<text class="name">在线签到</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-2.png" mode="widthFix" class="icon"></image>
+					<text class="name">员工健康</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-3.png" mode="widthFix" class="icon"></image>
+					<text class="name">在线请假</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-4.png" mode="widthFix" class="icon"></image>
+					<text class="name">公务出差</text>
+				</view>
+			</view>
+			<view class="nav-row">
+				<view class="nav">
+					<image src="../../static/nav-5.png" mode="widthFix" class="icon"></image>
+					<text class="name">员工日报</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-6.png" mode="widthFix" class="icon"></image>
+					<text class="name">我的加班</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-7.png" mode="widthFix" class="icon"></image>
+					<text class="name">付款申请</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-8.png" mode="widthFix" class="icon"></image>
+					<text class="name">费用报销</text>
+				</view>
+			</view>
+			<view class="nav-row">
+				<view class="nav">
+					<image src="../../static/nav-9.png" mode="widthFix" class="icon"></image>
+					<text class="name">公告通知</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-10.png" mode="widthFix" class="icon"></image>
+					<text class="name">在线审批</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-11.png" mode="widthFix" class="icon"></image>
+					<text class="name">物品领用</text>
+				</view>
+				<view class="nav">
+					<image src="../../static/nav-12.png" mode="widthFix" class="icon"></image>
+					<text class="name">采购申请</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -22,11 +90,11 @@
 	export default {
 		data() {
 			return {
-				
+				unreaderRows: 0
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
--- unpackage/dist/dev/mp-weixin/pages/login/login.js
+++ unpackage/dist/dev/mp-weixin/pages/login/login.js
@@ -1,5 +1,26 @@
 (global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/login/login"],{
 
+/***/ 34:
+/*!***********************************************************************!*\
+  !*** D:/uni-peoject/emos-wx/main.js?{"page":"pages%2Flogin%2Flogin"} ***!
+  \***********************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(wx, createPage) {
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ 4);
+__webpack_require__(/*! uni-pages */ 26);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 25));
+var _login = _interopRequireDefault(__webpack_require__(/*! ./pages/login/login.vue */ 35));
+// @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_login.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/wx.js */ 1)["default"], __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 2)["createPage"]))
+
+/***/ }),
+
 /***/ 35:
 /*!****************************************************!*\
   !*** D:/uni-peoject/emos-wx/pages/login/login.vue ***!
@@ -204,28 +225,7 @@ __webpack_require__.r(__webpack_exports__);
     if(false) { var cssReload; }
   
 
-/***/ }),
-
-/***/ 50:
-/*!***********************************************************************!*\
-  !*** D:/uni-peoject/emos-wx/main.js?{"page":"pages%2Flogin%2Flogin"} ***!
-  \***********************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-/* WEBPACK VAR INJECTION */(function(wx, createPage) {
-
-var _interopRequireDefault = __webpack_require__(/*! @babel/runtime/helpers/interopRequireDefault */ 4);
-__webpack_require__(/*! uni-pages */ 26);
-var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 25));
-var _login = _interopRequireDefault(__webpack_require__(/*! ./pages/login/login.vue */ 35));
-// @ts-ignore
-wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
-createPage(_login.default);
-/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/wx.js */ 1)["default"], __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 2)["createPage"]))
-
 /***/ })
 
-},[[50,"common/runtime","common/vendor"]]]);
+},[[34,"common/runtime","common/vendor"]]]);
 //# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/login/login.js.map
